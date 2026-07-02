1613af8fb97c8498def577da8bf003a0951d86b3
Manish Sharma <manishbhiwal02@gmailcom>
2026-07-02 16:02:05 -0400

    Fix port for Render deployment

--- 06_Web_Integration/app.py
+++ 06_Web_Integration/app.py
@@ -7,4 +7,5 @@ def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
